797aa849bd7a07d05acdba9889a4a4c69478baea
littleossa <littleossa@gmail.com>
2022-08-17 05:47:12 +0900

    Initial Commit

---
+++ VideoPlayerPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoPlayerPractice
+//
+//  Created by 平岡修 on 2022/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
